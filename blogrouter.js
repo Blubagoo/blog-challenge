60af48f83a492d8834076e1296bfcfea24f8b34c
Jim Tisdale <blubagoowenischinchilla@gmail.com>
2018-06-21 18:26:56 -0500

    finish v2

--- blogrouter.js
+++ blogrouter.js
@@ -12,34 +12,52 @@ BlogPosts.create({title:"Days Go By",
 router.get('/', (req,res) => {
 	res.json(BlogPosts.get());
 });
-router.post('/', (req, res) => {
-	const requiredFields =  ["title", "content", "author", "publishDate"];
-	console.log('req.body', req);
+
+function validateAPIRequest(req) {
+	const requiredFields =  ["title", "content", "author"];
+	
 	for(let i=0; i<requiredFields.length; i++){
 		const field = requiredFields[i];
+		
 		if(!(field in req.body)){
 			const message = `Missing \`${field}\` in request body`;
 			console.error(message);
-			return res.status(400).send(message);
+			
 		}
-		const item = BlogPosts.create(req.body.title, req.body.content, req.body.author, req.body.publishDate);
-		return res.status(201).json(item);
-	};
+	}	
+}
+
+
+
+
+router.post('/', (req, res) => {
+	
+	if(!validateAPIRequest(req)){
+		return res.status(400).send(message);
+	}
+	
+	const item = BlogPosts.create(req.body.title, req.body.content, req.body.author, req.body.publishDate);
+	return res.status(201).json();
+
 });
 
 router.put('/:id', (req, res) => {
-	const requiredFields = ["title", "content", "author", "publishDate"];
+	const requiredFields = ["title", "content", "author"];
+	
 	for(let i=0; i<requiredFields.length; i++){
 		const field = requiredFields[i];
+		
 		if(!(field in req.body)){
 			const message = `Missing \`${field}\` in request body`;
 			res.status(400).send(message);
 		}
 	}
+	
 	if(req.params.id !== req.body.id){
 		const message = `Request path id (${req.params.id}) must match request body id(${req.body.id})`;
 		res.status(400).send(message);
 	}
+	
 	console.log(`updating blog with id ${req.body.id}`);
 	BlogPosts.update({
 		"title": req.body.title,
